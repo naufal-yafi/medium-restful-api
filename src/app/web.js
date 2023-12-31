92e304d9e13a53d9fd7746a68854963167b49d46
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-12-31 11:21:14 +0700

    2.12 Membuat middleware error handler

--- src/app/web.js
+++ src/app/web.js
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express from "express";
+import ErrorMiddleware from "../middlewares/ErrorMiddleware.js";
 
 export const web = express();
 
 web.use(cors());
 web.use(express.json());
+web.use(ErrorMiddleware);
 
 web.use("", (req, res) =>
   res.json({
---
+++ src/middlewares/ErrorMiddleware.js
@@ -0,0 +1,26 @@
+import { ResponseError } from "../errors/ResponseError.js";
+
+const ErrorMiddleware = async (err, req, res, next) => {
+  if (!err) {
+    next();
+    return;
+  }
+
+  if (err instanceof ResponseError) {
+    res
+      .status(err.status)
+      .json({
+        errors: err.message,
+      })
+      .end();
+  } else {
+    res
+      .status(500)
+      .json({
+        errors: err.message,
+      })
+      .end();
+  }
+};
+
+export default ErrorMiddleware;
